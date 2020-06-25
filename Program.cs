3c019495e888dd6d0b0e1b63c916216a8081b573
Jack Gittoes <Jrgittoes@gmail.com>
2020-06-25 14:34:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    public static string QuestionsMarks(string str)
+    {
+        bool position = false;
+        int number = 0;
+
+        foreach (char i in str)
+        {
+           number++;
+           position =  Position(number, i);
+           if (number == 3 && position == false)
+            {
+                number = 0;
+            }
+           else if (position == true)
+            {
+                return position.ToString().ToLower();
+            }
+        }
+        return position.ToString().ToLower();
+
+
+    }
+    public static bool Position(int number, char letter)
+    {
+
+        if (letter == '?' && number == 3)
+        {    
+            return true;
+        }
+        return false;   
+    }
+
+    static void Main()
+    {
+        // keep this function call here
+        Console.WriteLine(QuestionsMarks(Console.ReadLine()));
+
+    }
+
+}
